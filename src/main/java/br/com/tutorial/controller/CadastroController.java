cb8ed996cac18555900884230e958fd9ae953da0
angelo <angelojrp@gmail.com>
2015-10-31 08:19:48 -0200

    aula

---
+++ src/main/java/br/com/tutorial/controller/CadastroController.java
@@ -0,0 +1,74 @@
+package br.com.tutorial.controller;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+import br.com.tutorial.model.CadastroDTO;
+
+
+@ManagedBean(name="cadastro")
+public class CadastroController {
+	
+	private CadastroDTO dto = new CadastroDTO();
+	
+
+
+public CadastroDTO getDto() {
+		return dto;
+	}
+
+
+public void setDto(CadastroDTO dto) {
+		this.dto = dto;
+	}
+
+
+
+
+
+
+
+
+
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
