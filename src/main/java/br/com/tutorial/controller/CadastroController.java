5373cccfa79e90bf16df4624577f32cbb001b885
Lilian <Lilian@Lilian-PC>
2015-11-06 00:13:59 -0200

    Aula 01 - Formulario com primafaces

--- src/main/java/br/com/tutorial/controller/CadastroController.java
+++ src/main/java/br/com/tutorial/controller/CadastroController.java
@@ -1,42 +1,61 @@
 package br.com.tutorial.controller;
 
-import java.util.Calendar;
-import java.util.Date;
-
-import javax.annotation.PostConstruct;
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
-import br.com.tutorial.model.CadastroDTO;
+import br.com.tutorial.modelo.Anuncio;
+
 
 
 @ManagedBean(name="cadastro")
+@ViewScoped
 public class CadastroController {
 	
-	private CadastroDTO dto = new CadastroDTO();
+	 private Anuncio anuncio = new Anuncio();
+	 
+	 
+	 public void cadastrar(){
+		 
+		 System.out.println("Nome:"+anuncio.getNome());
+		 System.out.println("E-mail:"+anuncio.getEmail());
+		 System.out.println("telefone:"+anuncio.getTelefone());
+		 System.out.println("titulo:"+anuncio.getTitulo());
+		 System.out.println("descrição:"+anuncio.getDescricao());
+		 System.out.println("CEP:"+anuncio.getCep());
+		 System.out.println("Senha:"+anuncio.getSenha());
+		 
+		 
+	 }
+	 
+	 
+
+
+
+
+	public Anuncio getAnuncio() {
+		return anuncio;
+	}
+
+
+
+	public void setAnuncio(Anuncio anuncio) {
+		this.anuncio = anuncio;
+	}
 	
-
-
-public CadastroDTO getDto() {
-		return dto;
-	}
-
-
-public void setDto(CadastroDTO dto) {
-		this.dto = dto;
-	}
-
-
-
-
-
-
-
-
-
-
 	
 }
+
+
+
+
+
+
+
+
+
+
+	
+
 
 
 
