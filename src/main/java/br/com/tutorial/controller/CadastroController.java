196b56b810552d24572dfaa128af3ddd71167de1
aluno.4rlab01 <aluno.4rlab02@4RLAB02-20.acadnet.aeudf.br>
2015-11-07 12:57:38 -0200

    Aula02

--- src/main/java/br/com/tutorial/controller/CadastroController.java
+++ src/main/java/br/com/tutorial/controller/CadastroController.java
@@ -1,93 +1,53 @@
 package br.com.tutorial.controller;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
 
-import br.com.tutorial.modelo.Anuncio;
+import br.com.tutorial.model.CadastroDTO;
+import br.com.tutorial.model.Categoria;
+import br.com.tutorial.model.UF;
+import br.com.tutorial.model.dao.CadastroDAO;
 
 
 
-@ManagedBean(name="cadastro")
-@ViewScoped
-public class CadastroController {
+@ManagedBean(name="cadastroController")
+@RequestScoped
+public class CadastroController implements Serializable {
 	
-	 private Anuncio anuncio = new Anuncio();
-	 
-	 
-	 public void cadastrar(){
-		 
-		 System.out.println("Nome:"+anuncio.getNome());
-		 System.out.println("E-mail:"+anuncio.getEmail());
-		 System.out.println("telefone:"+anuncio.getTelefone());
-		 System.out.println("titulo:"+anuncio.getTitulo());
-		 System.out.println("descrição:"+anuncio.getDescricao());
-		 System.out.println("CEP:"+anuncio.getCep());
-		 System.out.println("Senha:"+anuncio.getSenha());
-		 
-		 
-	 }
-	 
-	 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private CadastroDTO dto = new CadastroDTO();
 
 
-
-
-	public Anuncio getAnuncio() {
-		return anuncio;
+	public CadastroDTO getDto() {
+		return dto;
 	}
 
 
+	public void setDto(CadastroDTO dto) {
+		this.dto = dto;
+	}
 
-	public void setAnuncio(Anuncio anuncio) {
-		this.anuncio = anuncio;
+	public List<UF> getUfs(){
+		CadastroDAO dao = new CadastroDAO();
+		
+		return dao.getUfs();
 	}
 	
+	public List<Categoria> getCategorias(){
+		CadastroDAO dao = new CadastroDAO();
+		return dao.getCategorias();
+	}
+	
+	public String salvar(){
+		CadastroDAO dao = new CadastroDAO();
+		dao.salvarAnuncio(dto.getAnuncio());
+		return "index";
+	}
 	
 }
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
